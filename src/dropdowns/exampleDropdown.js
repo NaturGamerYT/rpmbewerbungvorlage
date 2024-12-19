9384b870e007c833c41583775281749545ed8f98
NaturGamerYT <86455598+NaturGamerYT@users.noreply.github.com>
2024-12-19 19:19:47 +0100

    Update exampleDropdown.js

--- src/dropdowns/exampleDropdown.js
+++ src/dropdowns/exampleDropdown.js
@@ -1,7 +1,6 @@
 module.exports = {
     customId: 'exampleDropdown',
     async execute(interaction, client) {
-        // Hier kannst du deine Logik für die Dropdown-Interaktion implementieren
         await interaction.reply({
             content: 'Option aus Dropdown ausgewählt!',
             ephemeral: true
